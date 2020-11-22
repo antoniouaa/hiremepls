edbcc92d095fa67bf7dab12caf62874814c8d503
antoniouaa <antoniouaa@hotmail.com>
2020-11-22 16:52:48 +0000

    small changes in style

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "react-bootstrap";
@@ -8,9 +8,67 @@ import "./App.css";
 import Sidebar from "./Components/Sidebar";
 import Homepage from "./Components/Body/Homepage";
 import Contact from "./Components/Body/Contact";
+import Project from "./Components/Body/Project";
 import ProjectsList from "./Components/Body/ProjectsList";
 
 function App() {
+  const [projects, setProjects] = useState([]);
+
+  const TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+  const ENDPOINT = process.env.REACT_APP_GITHUB_ENDPOINT;
+
+  // eslint-disable-next-line
+  const fetchConfig = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${TOKEN}`,
+    },
+    body: JSON.stringify({
+      query: `
+        query {
+          user(login: "antoniouaa") {
+            pinnedItems(first: 5, types: [REPOSITORY]) {
+              totalCount
+              edges {
+                node {
+                  ... on Repository {
+                    name
+                    description
+                    createdAt
+                    url
+                    pushedAt
+                    primaryLanguage {
+                      name
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      `,
+    }),
+  };
+
+  // eslint-disable-next-line
+  const fetchPins = (options) => {
+    return fetch(ENDPOINT, options)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setProjects(data.data.user.pinnedItems.edges);
+      });
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetchPins(fetchConfig);
+    }, 1000);
+  }, []);
+
+  console.log(projects);
+
   return (
     <Router>
       <div className="App">
@@ -18,8 +76,13 @@ function App() {
         <div className="App Wrapper">
           <Switch>
             <Route path="/home" component={Homepage} />
-            <Route path="/projects" component={ProjectsList} />
+            <Route path="/projects">
+              <ProjectsList projs={projects} />
+            </Route>
             <Route path="/contact" component={Contact} />
+            <Route path="/projects/:project-name">
+              <Project projects={projects} />
+            </Route>
             <Route path="/" component={Homepage} />
           </Switch>
         </div>
--- src/Components/Body/ProjectsList.js
+++ src/Components/Body/ProjectsList.js
@@ -1,96 +1,32 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 import Project from "./Project";
-import { projs } from "./p";
 
 import "./Project.css";
 
-export default function ProjectsList(props) {
-  const TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
-  const ENDPOINT = process.env.REACT_APP_GITHUB_ENDPOINT;
-  const [projects, setProjects] = useState([]);
-
-  // eslint-disable-next-line
-  const fetchConfig = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${TOKEN}`,
-    },
-    body: JSON.stringify({
-      query: `
-        query {
-          user(login: "antoniouaa") {
-            pinnedItems(first: 5, types: [REPOSITORY]) {
-              totalCount
-              edges {
-                node {
-                  ... on Repository {
-                    name
-                    description
-                    createdAt
-                    url
-                    pushedAt
-                    primaryLanguage {
-                      name
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      `,
-    }),
-  };
-
-  // eslint-disable-next-line
-  const fetchPins = (options) => {
-    return (
-      fetch(ENDPOINT, options)
-        .then((res) => res.json())
-        // .then((data) => setProjects(data.data.pinnedItems.edges))
-        .then((data) => console.log(data.data.pinnedItems.edges))
-    );
-  };
-
-  // eslint-disable-next-line
-  const checkRateLimit = () => {
-    return fetch("https://api.github.com/rate_limit")
-      .then((res) => res.json())
-      .then((d) => console.log(d));
-  };
-
-  useEffect(() => {
-    setTimeout(() => {
-      setProjects(projs);
-    }, 100);
-  }, [setProjects]);
-
+export default function ProjectsList({ projs }) {
   return (
     <div className="project-wrapper">
-      <div className="project-row">
-        {projects.map((p) => {
-          const {
-            name,
-            description,
-            createdAt,
-            pushedAt,
-            url,
-            primaryLanguage,
-          } = p.node;
-          return (
-            <Project
-              name={name}
-              description={description}
-              createdAt={createdAt}
-              pushedAt={pushedAt}
-              url={url}
-              primaryLanguage={primaryLanguage.name}
-            />
-          );
-        })}
-      </div>
+      {projs.map((p) => {
+        const {
+          name,
+          description,
+          createdAt,
+          pushedAt,
+          url,
+          primaryLanguage,
+        } = p.node;
+        return (
+          <Project
+            name={name}
+            description={description}
+            createdAt={createdAt}
+            pushedAt={pushedAt}
+            url={url}
+            primaryLanguage={primaryLanguage.name}
+          />
+        );
+      })}
     </div>
   );
 }
