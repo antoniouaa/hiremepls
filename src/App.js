93490ca68a1a550a6770633dc10565aa2a9f8957
alex antoniou <alexandros@ingage.com>
2022-01-29 00:50:33 +0000

    run create-react-app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+// #8fb9a8
+// #fefad4
+// #fcd0ba
+// #f1828d
+// #765d69
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
