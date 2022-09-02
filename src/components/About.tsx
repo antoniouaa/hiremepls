0af87b2d6a5a04db8175a1d5e1368227f8620e2f
antoniouaa <antoniouaa@hotmail.com>
2022-09-02 23:09:21 +0100

    updated about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,13 +1,14 @@
-import { RepoLink } from "./Card"
+import { RepoLink as Link } from "./Card"
 import { Container } from "./common/styled";
 
-const colored = (color: string, text: string) => <span style={{ color }}>{text}</span>
 
 const About = () => {
+    const colored = (color: string, text: string) => <span style={{ color }}>{text}</span>
+
     return (
         <Container width="100%">
-            <p>Software developer currently using {colored("blue", "Python")}, {colored("green", "VBA")} and {colored("purple", "PowerShell")} to maintain and develop internal and client-facing reporting tools for <RepoLink href="https://www.ingage.com/">{colored("teal", "ingage IR")}</RepoLink>.</p>
-            <p>Learning {colored("palevioletred", "C#")} and {colored("steelblue", "TypeScript")}.</p><br />
+            <p>Software engineer currenly using {colored("blue", "Python")}, {colored("darkorange", "JavaScript")} and related technologies to maintain and develop client-facing and administrative UI for <Link href="https://www.playtech.com/">{colored("darkblue", "Playtech")}</Link>.</p>
+            <p>Learning {colored("steelblue", "TypeScript")} and {colored("brown", "Rust")}.</p><br />
         </Container>)
 }
 
